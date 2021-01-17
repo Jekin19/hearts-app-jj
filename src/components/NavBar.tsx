ad43cf97083d5cd488ac4355e619b9a4495847ca
jillika <jillika@gmail.com>
2021-01-16 22:38:23 -0500

    Updated Font Color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,9 +7,9 @@ function NavBar() {
       <Navbar bg="dark" variant="dark" className="p-0 pl-3">
         <Navbar.Brand href="#home">
           <img src="./heart.ico" width="30" height="30" alt=""></img>
+          
         </Navbar.Brand>
         <Form inline>
-          <h2 className="heart-color font-weight-bold ">Hearts </h2>
           <Button className="ml-3" variant="outline-success">
             New Game
           </Button>
