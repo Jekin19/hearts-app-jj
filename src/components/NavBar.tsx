30a53cb2ba4b87a2905b11fd7ad99553ab60a984
Jekin Sanghavi <sjekin@amazon.com>
2021-01-16 21:37:23 -0500

    Setup with React, React-Bootstrap, Sass, TS.

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Form, Navbar } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" className="p-0 pl-3">
+        <Navbar.Brand href="#home">
+          <h2 className="text-danger font-weight-bold ">Hearts </h2>
+        </Navbar.Brand>
+        <Form inline>
+          <Button className="ml-3" variant="outline-success">
+            New Game
+          </Button>
+        </Form>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
