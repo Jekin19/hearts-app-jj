ce72980a2bb32a15bf17c5ace21f829576f4cd6b
Jekin Sanghavi <sjekin@amazon.com>
2021-01-16 23:24:58 -0500

    Add Hearts game play area and background image.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { HeartsGame } from "./components/HeartsGames";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <HeartsGame />
     </>
   );
 }
---
+++ src/components/HeartsGames.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const HeartsGame: React.FC = () => {
+  return (
+    <Container fluid className="content" style={{ height: "calc(100% - 56px)" }}>
+      <div> Hearts game goes here...</div>
+    </Container>
+  );
+};
